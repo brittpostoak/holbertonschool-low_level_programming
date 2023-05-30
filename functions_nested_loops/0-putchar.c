e79203322aa0ef699b46550b5e6df5fd9de1bfec
Brittany_Herman <brittherman86@gmail.com>
2023-05-30 16:06:44 -0700

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "_putchar.h"
-	/**
-	 * main - print _putchar
-	 * This function is the entry point for the program.
-	 * It prints the message "_putchar" to the console.
-	 * Return: 0
-	 */
 
-
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
