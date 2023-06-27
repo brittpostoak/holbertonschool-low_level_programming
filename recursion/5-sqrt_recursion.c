b0ee2333e0159756eb23c0761ba69139b95910a6
Brittany_Herman <brittherman86@gmail.com>
2023-06-26 20:41:56 -0700

    recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -19,7 +19,7 @@ int _sqrt_recursion(int n)
  * @n: number to calculate the square root
  * @i: iterate number
  *
- * Return : the natural square root
+ * Return: the natural square root
  */
 int _sqrt(int n, int i)
 {
