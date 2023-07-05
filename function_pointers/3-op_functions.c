e3bd3de736200710c212e21d5d7644f3c907cc3b
Brittany_Herman <brittherman86@gmail.com>
2023-07-04 23:46:38 -0700

    function pointers

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -42,7 +42,7 @@ int op_mul(int a, int b)
 int op_div(int a, int b)
 {
 	if (b == 0)
-		return (-1);
+		return (0);
 	return (a / b);
 }
 
@@ -55,6 +55,6 @@ int op_div(int a, int b)
 int op_mod(int a, int b)
 {
 	if (b == 0)
-		return (-1);
+		return (0);
 	return (a % b);
 }
